1f5e51a31cb5f1de7d52addf3a903a02d8a910ca
ov3rst <otejeda41@gmail.com>
2023-06-12 18:43:42 -0400

    menu mobile arreglado

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ console.log("Listo");
 
 const d = document,
   $menu = d.getElementById("menu"),
-  $btn = d.getElementById("menu-btn"),
   $openIcon = d.querySelector(".open__icon"),
   $closeIcon = d.querySelector(".close__icon");
 
 d.addEventListener("click", (e) => {
-  if (e.target.matches(`${$btn} *`)) {
-    $menu.classList.toggle(".is-active");
+  if (e.target.matches(`.menu__mobile-btn *`)) {
+    console.log("entre");
+    $menu.classList.toggle("is-active");
+    $openIcon.classList.toggle("open-close");
+    $closeIcon.classList.toggle("open-close");
   }
 });
