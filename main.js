9ace03e4f1c9e0f5feb3cb3547d8000849735e0a
ov3rst <otejeda41@gmail.com>
2023-06-17 12:38:30 -0400

    ultimos retoques al header y comenzando con el html de la seccion humanizing

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-console.log("Listo");
-
 const d = document,
   $menu = d.getElementById("menu"),
   $openIcon = d.querySelector(".open__icon"),
@@ -7,7 +5,6 @@ const d = document,
 
 d.addEventListener("click", (e) => {
   if (e.target.matches(`.menu__mobile-btn *`)) {
-    console.log("entre");
     $menu.classList.toggle("is-active");
     $openIcon.classList.toggle("open-close");
     $closeIcon.classList.toggle("open-close");
