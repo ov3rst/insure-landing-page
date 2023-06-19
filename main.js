710818cb098ce30a8a28516a093b9ad48a1d4ab2
Oliver Tejeda <oliver@hot.com>
2023-06-18 23:38:25 -0400

    pagina completada

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const d = document,
+  $header = d.getElementById("header"),
   $menu = d.getElementById("menu"),
   $openIcon = d.querySelector(".open__icon"),
   $closeIcon = d.querySelector(".close__icon");
 
 d.addEventListener("click", (e) => {
   if (e.target.matches(`.menu__mobile-btn *`)) {
+    $header.classList.toggle("sticky");
     $menu.classList.toggle("is-active");
     $openIcon.classList.toggle("open-close");
     $closeIcon.classList.toggle("open-close");
